001b073741d17e5f2d6c0e2b0078aa9a0bd49a4c
cls688 <1536463948@qq.com>
2021-04-22 20:16:54 +0800

    DateTimeFormatter

--- JavaSenior/Day04/src/com/atguigu/java/JDK8DateTimeTest.java
+++ JavaSenior/Day04/src/com/atguigu/java/JDK8DateTimeTest.java
@@ -3,6 +3,9 @@ package com.atguigu.java;
 import org.junit.Test;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
 import java.util.Date;
 
 /**
@@ -25,7 +28,7 @@ public class JDK8DateTimeTest {
      */
 
     @Test
-    public void test1(){
+    public void test1() {
 
         // now() 获取当前的日期、时间、日期+时间
         LocalDate localDate = LocalDate.now();
@@ -60,7 +63,7 @@ public class JDK8DateTimeTest {
      */
 
     @Test
-    public void test2(){
+    public void test2() {
         Instant instant = Instant.now();
         System.out.println(instant);
         // 时间偏移量
@@ -74,5 +77,42 @@ public class JDK8DateTimeTest {
         // 通过给定的毫秒数，获取Instant实例-->Date(long millis)
         Instant instant1 = Instant.ofEpochMilli(1618925531763L);
         System.out.println(instant1);
+    }
+
+    /*
+    DateTimeFormatter
+     */
+    @Test
+    public void test3() {
+        //方式一：预定义的标准格式。如，ISO_LOCAL_DATE_TIME;ISO_LOCAL_DATE;ISO_LOCAL_TIME
+        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+        // 格式化：日期-->字符串
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String str1 = formatter.format(localDateTime);
+//        System.out.println(localDateTime);
+        System.out.println(str1);
+
+        // 解析：字符串-->日期
+        TemporalAccessor parse = formatter.parse("2021-04-22T19:46:37.916");
+        System.out.println(parse);
+
+        // 方式二：本地化相关的格式。如：ofLocalizedDateTime(FormatStyle.LONG)
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
+        String str2 = formatter1.format(localDateTime);
+        System.out.println(str2);
+
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        // 格式化
+        String str3 = formatter2.format(LocalDate.now());
+        System.out.println(str3);
+
+        // 方式三：自定义的格式。如：ofPattern(“yyyy-MM-dd hh:mm:ss”)
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+        // 格式化
+        String str4 = formatter3.format(LocalDateTime.now());
+        System.out.println(str4);//2021-04-22 08:14:36
+
+        TemporalAccessor accessor = formatter3.parse("2021-04-22 08:14:36");
+        System.out.println(accessor);
     }
 }
