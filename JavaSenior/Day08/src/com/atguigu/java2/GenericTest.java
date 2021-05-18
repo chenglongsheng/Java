dbcc699a6f55b74fa1dcc091a4761dcedbc4281b
cls688 <1536463948@qq.com>
2021-05-18 17:12:22 +0800

    通配符的使用

--- JavaSenior/Day08/src/com/atguigu/java2/GenericTest.java
+++ JavaSenior/Day08/src/com/atguigu/java2/GenericTest.java
@@ -3,12 +3,13 @@ package com.atguigu.java2;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * 1.泛型在继承方面的体现
- *      类A是类B的父类，C<A> 和 C<B> 二者不具备子父类关系，二者是并列关系
- *
+ * 类A是类B的父类，C<A> 和 C<B> 二者不具备子父类关系，二者是并列关系
+ * <p>
  * 2.通配符的使用
  *
  * @author chenglongsheng
@@ -35,5 +36,32 @@ public class GenericTest {
         List<String> list2 = null;
 //        list1 = list2;   // 编译不通过
     }
+
+    /*
+    通配符的使用"?"
+     */
+    @Test
+    public void test2() {
+        List<Object> list1 = null;
+        List<String> list2 = null;
+
+        List<?> list = null;
+
+        list = list1;
+        list = list2;
+
+        print(list1);
+        print(list2);
+    }
+
+    public void print(List<?> list) {
+        Iterator<?> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+
+            System.out.println(next);
+        }
+    }
+
 
 }
