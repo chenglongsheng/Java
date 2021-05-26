1499e650fc267c78f881302a492b0b6b6382c268
cls688 <1536463948@qq.com>
2021-05-26 19:57:16 +0800

    FileWriter写数据

--- JavaSenior/Day09/src/com/atguigu/java/FileReaderWriter.java
+++ JavaSenior/Day09/src/com/atguigu/java/FileReaderWriter.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -85,6 +86,33 @@ public class FileReaderWriter {
             }
         }
 
+
+    }
+
+    /*
+    从内存中写出数据到硬盘的文件里
+
+    1.输出操作，对应File可以不存在
+        如果不存在，在输出过程中，会自动创建此文件
+        如果存在，
+            如果流使用的构造器是FileWriter(file,false)/(file)对原有文件覆盖
+            如果流使用的构造器是FileWriter(file,true)不会对原有文件覆盖，而是在原有文件基础上追加内容
+     */
+    @Test
+    public void testFileWriter() throws IOException {
+        // 1.提供File类的对象，指明写出到的文件
+        File file = new File("hello1.text");
+
+        // 2.提供FileWriter的对象，用于数据的写出
+//        FileWriter fw = new FileWriter(file);
+        FileWriter fw = new FileWriter(file,true);
+
+        // 3.写出的操作
+        fw.write("I have a dream!\n");
+        fw.write("you need to have a dream!");
+
+        // 4.流资源的关闭
+        fw.close();
 
     }
 
