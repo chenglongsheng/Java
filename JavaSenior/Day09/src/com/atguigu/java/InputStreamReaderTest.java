f22cecdd29c1db7447e26de687e3837d56f34583
cls688 <1536463948@qq.com>
2021-05-29 22:01:34 +0800

    实现字节的输入流到字符输入流的转换

---
+++ JavaSenior/Day09/src/com/atguigu/java/InputStreamReaderTest.java
@@ -0,0 +1,55 @@
+package com.atguigu.java;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * 处理流之二：转换流
+ * 1.转换流：属于字符流
+ * InputStreamReader：将一个字节输入流转换为字符的输出流
+ * OutputStreamWriter：将一个字符的输出流转换为字节的输出流
+ * 2.作用：提供字节流与字符流之间的转换
+ * <p>
+ * 3.解码：字节、字节数组---->字符、字符数组
+ * 编码：字符、字符数组---->字节、字节数组
+ * 4.字符集
+ *
+ * @author chenglongsheng
+ * @create 2021-05-29 21:38
+ */
+public class InputStreamReaderTest {
+
+    @Test
+    /*
+    InputStreamReader：实现字节的输入流到字符输入流的转换
+     */
+    public void test1() {
+
+        InputStreamReader isr = null;//指明字符集
+        try {
+            FileInputStream fis = new FileInputStream("dbcp.txt");
+//        InputStreamReader isr = new InputStreamReader(fis);//使用系统默认字符集
+            isr = new InputStreamReader(fis, "utf-8");
+
+            char[] cbuf = new char[1024];
+            int len;
+            while ((len = isr.read(cbuf)) != -1) {
+                String str = new String(cbuf, 0, len);
+                System.out.print(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+}
