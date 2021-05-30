181d5e9c1105e0b1d3c215159763f403c913e714
cls688 <1536463948@qq.com>
2021-05-30 21:40:06 +0800

    转换流实现文件的读入和写出

--- JavaSenior/Day09/src/com/atguigu/java/InputStreamReaderTest.java
+++ JavaSenior/Day09/src/com/atguigu/java/InputStreamReaderTest.java
@@ -52,4 +52,48 @@ public class InputStreamReaderTest {
 
     }
 
+    /*
+    综合使用InputStreamReader和OutputStreamWriter
+     */
+    @Test
+    public void test2() {
+        InputStreamReader isr = null;
+        OutputStreamWriter osw = null;
+        try {
+            File file1 = new File("dbcp.txt");
+            File file2 = new File("dbcp_gbk.txt");
+
+            FileInputStream fis = new FileInputStream(file1);
+            FileOutputStream fos = new FileOutputStream(file2);
+
+            isr = new InputStreamReader(fis, "utf-8");
+            osw = new OutputStreamWriter(fos, "gbk");
+
+            char[] cbuf = new char[20];
+            int len;
+            while ((len = isr.read(cbuf)) != -1) {
+                osw.write(cbuf, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (isr != null) {
+                try {
+                    isr.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (osw != null) {
+                try {
+                    osw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
 }
