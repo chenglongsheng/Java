92889b8d9cd236cfaf06491991d984c4d0c339d9
cls688 <1536463948@qq.com>
2021-06-07 21:38:42 +0800

    TCP网络编程例题3

---
+++ JavaSenior/Day10/src/com/atguigu/java/TCPTest3.java
@@ -0,0 +1,157 @@
+package com.atguigu.java;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-07 19:58
+ */
+public class TCPTest3 {
+
+    @Test
+    public void client() {
+
+        Socket socket = null;
+        OutputStream os = null;
+        FileInputStream fis = null;
+        ByteArrayOutputStream baos = null;
+        InputStream is = null;
+        try {
+            socket = new Socket(InetAddress.getByName("127.0.0.1"), 9900);
+            os = socket.getOutputStream();
+            fis = new FileInputStream(new File("girl.jpg"));
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, len);
+            }
+
+            socket.shutdownOutput();
+
+            // 接受来自服务端的数据，并显示在控制台
+            is = socket.getInputStream();
+            baos = new ByteArrayOutputStream();
+            byte[] buffer1 = new byte[20];
+            int len1;
+            while ((len1 = is.read(buffer1)) != -1) {
+                baos.write(buffer1, 0, len1);
+            }
+
+            System.out.println(baos.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+    @Test
+    public void server() {
+
+        ServerSocket ss = null;
+        Socket socket = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        OutputStream os = null;
+        try {
+            ss = new ServerSocket(9900);
+            socket = ss.accept();
+            is = socket.getInputStream();
+            fos = new FileOutputStream(new File("girl2.jpg"));
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+
+            os = socket.getOutputStream();
+            os.write("照片我已收到".getBytes());
+
+            System.out.println("接收成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
+}
