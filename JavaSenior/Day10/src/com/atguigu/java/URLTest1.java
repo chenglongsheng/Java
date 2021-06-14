96486ef164dcaeb7414e42f89b21e1a27369e55f
cls688 <1536463948@qq.com>
2021-06-14 21:50:04 +0800

    URL实现从tomcat服务端数据下载

---
+++ JavaSenior/Day10/src/com/atguigu/java/URLTest1.java
@@ -0,0 +1,62 @@
+package com.atguigu.java;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author chenglongsheng
+ * @create 2021-06-14 21:29
+ */
+public class URLTest1 {
+
+    public static void main(String[] args) {
+
+        HttpURLConnection urlConnection = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        try {
+            URL url = new URL("http://localhost:8080/examples/beauty.jpg");
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            is = urlConnection.getInputStream();
+            fos = new FileOutputStream("Day10\\beauty1.jpg");
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                fos.write(buffer, 0, len);
+            }
+            System.out.println("下载完成！");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+
+    }
+
+}
