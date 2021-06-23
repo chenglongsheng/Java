4b24e576cf086e2411d8d2a916f3c3a441007bb3
cls688 <1536463948@qq.com>
2021-06-23 21:08:45 +0800

    通过反射创建运行时类的对象

---
+++ JavaSenior/Day11/src/com/atguigu/java/NewInstanceTest.java
@@ -0,0 +1,34 @@
+package com.atguigu.java;
+
+import org.junit.Test;
+
+/**
+ * 通过发射创建对应的运行时类的对象
+ *
+ * @author chenglongsheng
+ * @create 2021-06-23 20:48
+ */
+public class NewInstanceTest {
+
+    @Test
+    public void test1() throws IllegalAccessException, InstantiationException {
+
+        Class<Person> clazz = Person.class;
+
+        /*
+        newInstance()：调用此方法，创建对应的运行时类对象。内部调用了运行时类的空参的构造器。
+
+        要想此方法正常的创建运行时类的对象，要求：
+        1.运行时类必须提供空参的构造器
+        2.空参的构造器的访问权限得够。通常，设置为public。
+
+        在javabean中要求提供一个public的空参构造器。原因：
+        1.便于通过反射，创建运行时类对象
+        2.便于子类继承此运行时类时，默认调用super()时，保证父类有此构造器
+         */
+        Person obj = clazz.newInstance();
+        System.out.println(obj);
+
+    }
+
+}
