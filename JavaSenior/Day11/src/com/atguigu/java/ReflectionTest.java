2299d9f6eb946fd3c6b5a3b1a385abee55da0f8c
cls688 <1536463948@qq.com>
2021-06-23 15:52:35 +0800

    获取Class实例

--- JavaSenior/Day11/src/com/atguigu/java/ReflectionTest.java
+++ JavaSenior/Day11/src/com/atguigu/java/ReflectionTest.java
@@ -18,7 +18,7 @@ public class ReflectionTest {
     @Test
     public void test1() {
 
-        Person p1 = new Person("TOm",12);
+        Person p1 = new Person("TOm", 12);
         p1.age = 10;
         System.out.println(p1.toString());
 
@@ -43,7 +43,7 @@ public class ReflectionTest {
         // 2.通过反射，调用对象指定的属性、方法
         // 调用属性
         Field age = personClass.getDeclaredField("age");
-        age.set(p,10);
+        age.set(p, 10);
         System.out.println(p.toString());
 
         // 调用方法
@@ -62,13 +62,56 @@ public class ReflectionTest {
         // 调用私有属性
         Field name = personClass.getDeclaredField("name");
         name.setAccessible(true);
-        name.set(p1,"Hanmeimei");
+        name.set(p1, "Hanmeimei");
         System.out.println(p1);
 
         // 调用私有方法
         Method showNation = personClass.getDeclaredMethod("showNation", String.class);
         showNation.setAccessible(true);
-        showNation.invoke(p1,"中国");// 相当于p1.showNation("中国")
+        showNation.invoke(p1, "中国");// 相当于p1.showNation("中国")
+
+
+    }
+
+    /*
+        java.lang.Class理解
+        1.类的加载过程：
+        程序经过javac.exe命令以后，会生成一个或多个字节码文件(.class结尾)。
+        接着使用java.exe命令对某个字节码文件进行解释运行。
+        相当于将某个字节码文件加载到内存中。
+        此过程就称为类的加载。
+        加载到内存中的类，就称为运行时类，此运行时类，就作为Class类的一个实例。
+
+        2.换句话说，Class的实例就对应着一个运行时类。
+        3.加载到内存中的运行时类，会缓存一定的时间。
+        在此时间之内，可以通过不同的方式来获取此运行时类。
+    */
+    // 获取Class的实例的方法
+    @Test
+    public void test3() throws ClassNotFoundException {
+        // 方式一：调用运行类的属性（.class）
+        Class clazz1 = Person.class;
+        System.out.println(clazz1);
+
+        // 方式二：调用运行时类的对象
+        Person p1 = new Person();
+        Class clazz2 = p1.getClass();
+        System.out.println(clazz2);
+
+        // 方式三：调用Class的静态方法：forName(String ClassPath)
+        // 可以在运行时报错
+        Class clazz3 = Class.forName("com.atguigu.java.Person");
+//        Class clazz3 = Class.forName("java.lang.String");
+        System.out.println(clazz3);
+
+        System.out.println(clazz1 == clazz2);
+        System.out.println(clazz1 == clazz3);
+
+        // 方式四：使用类的加载器：ClassLoader
+        ClassLoader classLoader = ReflectionTest.class.getClassLoader();
+        Class clazz4 = classLoader.loadClass("com.atguigu.java.Person");
+        System.out.println(clazz4);
+        System.out.println(clazz1 == clazz4);
 
     }
 
