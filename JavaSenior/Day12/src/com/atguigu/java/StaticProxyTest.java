e5797152bb0df02fb58e5649be963321208f8a87
cls688 <1536463948@qq.com>
2021-06-24 21:39:24 +0800

    静态代理示例

---
+++ JavaSenior/Day12/src/com/atguigu/java/StaticProxyTest.java
@@ -0,0 +1,50 @@
+package com.atguigu.java;
+
+/**
+ * 静态代理：代理类和被代理类在编译期间，就确定下来了
+ *
+ * @author chenglongsheng
+ * @create 2021-06-24 21:22
+ */
+interface ClothFactory {
+
+    void produceCloth();
+
+}
+
+// 代理类
+class ProxyClothFactory implements ClothFactory {
+
+    private ClothFactory factory;// 用被代理类对象进行实例化
+
+    public ProxyClothFactory(ClothFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void produceCloth() {
+        System.out.println("代理工厂做一些准备工作");
+        factory.produceCloth();
+        System.out.println("代理工厂做一些后续收尾工作");
+    }
+}
+
+// 被代理类
+class NikeClothFactory implements ClothFactory {
+
+    @Override
+    public void produceCloth() {
+        System.out.println("Nike工厂生产一批运动服");
+    }
+}
+
+public class StaticProxyTest {
+    public static void main(String[] args) {
+        // 创建被代理类对象
+        NikeClothFactory nike = new NikeClothFactory();
+
+        // 创建代理类对象
+        ProxyClothFactory proxyClothFactory = new ProxyClothFactory(nike);
+        proxyClothFactory.produceCloth();
+    }
+}
