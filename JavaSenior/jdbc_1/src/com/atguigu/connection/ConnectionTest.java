2fd1171fbde1b79d3d7d238f0ac08f4d8bd08810
cls688 <1536463948@qq.com>
2021-06-25 22:48:17 +0800

    获取连接方式二

--- JavaSenior/jdbc_1/src/com/atguigu/connection/ConnectionTest.java
+++ JavaSenior/jdbc_1/src/com/atguigu/connection/ConnectionTest.java
@@ -39,4 +39,28 @@ public class ConnectionTest {
 
     }
 
+    /*
+    方式二：对方式一的迭代
+    不出现第三方api,更好的移植性
+     */
+    @Test
+    public void testConnection2() throws ClassNotFoundException, IllegalAccessException, InstantiationException, SQLException {
+        // 1.或取Driver实现类对象，使用反射
+        Class clazz = Class.forName("com.mysql.jdbc.Driver");
+        Driver driver = (Driver) clazz.newInstance();
+
+        // 2.提供要连接的数据库
+        String url = "jdbc:mysql://localhost:3306/test";
+
+        // 3.提供连接需要的用户名和密码
+        Properties info = new Properties();
+        info.setProperty("user", "root");
+        info.setProperty("password", "123456");
+
+        // 4.获取连接
+        Connection conn = driver.connect(url, info);
+
+        System.out.println(conn);
+    }
+
 }
